372bdf3efa5d4f97f60c5b958a43eb22de46883e
robinhood0107 <pjj4678@gmail.com>
2026-08-22 00:01:43 +0900

    test(S6.7): V79 migration ceiling 동기화

--- workspaces/decision-platform/spring-api/src/test/kotlin/com/capstone/decision/RagSourceRegistryMigrationIntegrationTest.kt
+++ workspaces/decision-platform/spring-api/src/test/kotlin/com/capstone/decision/RagSourceRegistryMigrationIntegrationTest.kt
@@ -629,7 +629,7 @@ class RagSourceRegistryMigrationIntegrationTest {
                 assertThat(queryStrings(connection, normalizedTableQuery))
                     .containsAll(expectedTables)
                 assertThat(queryString(connection, "select max(version::integer) from flyway_schema_history where success"))
-                    .isEqualTo("78")
+                    .isEqualTo("79")
 
                 expectedTables.forEach { table ->
                     assertThat(
